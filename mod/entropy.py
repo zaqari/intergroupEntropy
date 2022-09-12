48fc1df9d688199c34ad851aef4fd2606d16325e
Zachary P Rosen <zrosen@uci.edu>
2022-09-12 10:40:52 -0700

    updated ./mod files including updates to H.py for CRON job that deploys the entropy class directly on CUDA enabled server.

--- mod/entropy.py
+++ mod/entropy.py
@@ -3,27 +3,37 @@ import torch.nn as nn
 
 class entropy(nn.Module):
     
-    def __init__(self, sigma=.3):
+    def __init__(self, sigma=.3, dim=None):
         super(entropy, self).__init__()
         self.cos = nn.CosineSimilarity(dim=-1)
-        self.N = torch.distributions.Normal(1,scale=sigma,validate_args=False)
-        self.condense_side = 'right'
+        self.N = torch.distributions.Normal(1,scale=sigma, validate_args=False)
+        self.dim = dim
 
-    def forward(self, ex, ey, condense_side=None):
-        if bool(side):
-            self.condense_side=condense_side
+    def forward(self, ex, ey, dim=None):
+        if bool(dim):
+            self.dim=dim
 
         C = self.cos(ex.unsqueeze(1), ey)
 
-        if self.condense_side == 'right':
-            C = self.N.log_prob(C.max(dim=-1).values)
-            return -(torch.exp(C) * C).sum()
-
-        elif self.condense_side == 'left':
-            C = self.N.log_prob(C.max(dim=0).values)
+        if self.dim != None:
+            C = self.N.log_prob(C.max(dim=self.dim).values)
             return -(torch.exp(C) * C).sum()
 
         else:
-            C1, C2 = self.N.log_prob(C.max(dim=-1)), self.N.log_prob(C.max(dim=0))
+            C1, C2 = self.N.log_prob(C.max(dim=-1).values), self.N.log_prob(C.max(dim=0).values)
             return -(torch.exp(C1) * C1).sum(), -(torch.exp(C2) * C2).sum()
 
+    def unsummed(self,ex,ey,dim=None):
+        if bool(dim):
+            self.dim=dim
+
+        C = self.cos(ex.unsqueeze(1), ey)
+
+        C = self.N.log_prob(C.max(dim=self.dim).values)
+        return -(torch.exp(C) * C)
+
+    def on_indexes(self, ex, ey, x_indeces, y_indeces):
+        C = self.cos(ex.unsqueeze(1), ey)
+
+        C1, C2 = self.N.log_prob(C.max(dim=-1).values[x_indeces]), self.N.log_prob(C.max(dim=0).values[y_indeces])
+        return -(torch.exp(C1) * C1).sum(), -(torch.exp(C2) * C2).sum()
