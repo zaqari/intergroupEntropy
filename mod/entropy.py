7e9c9e7786a154fc2771f7610af30aecc2e6cc77
Zachary P Rosen <zrosen@uci.edu>
2023-10-23 10:07:22 -0700

    Update entropy.py

--- mod/entropy.py
+++ mod/entropy.py
@@ -16,11 +16,11 @@ class entropy(nn.Module):
         C = self.cos(ex.unsqueeze(1), ey)
 
         if self.dim != None:
-            C = self.N.log_prob(C.max(dim=self.dim).values)
+            C = self.N.log_prob(1+C.max(dim=self.dim).values)
             return -(torch.exp(C) * C).sum()
 
         else:
-            C1, C2 = self.N.log_prob(C.max(dim=-1).values), self.N.log_prob(C.max(dim=0).values)
+            C1, C2 = self.N.log_prob(1+C.max(dim=-1).values), self.N.log_prob(1+C.max(dim=0).values)
             return -(torch.exp(C1) * C1).sum(), -(torch.exp(C2) * C2).sum()
 
     def unsummed(self,ex,ey,dim=None):
@@ -29,11 +29,11 @@ class entropy(nn.Module):
 
         C = self.cos(ex.unsqueeze(1), ey)
 
-        C = self.N.log_prob(C.max(dim=self.dim).values)
+        C = self.N.log_prob(1+C.max(dim=self.dim).values)
         return -(torch.exp(C) * C)
 
     def on_indexes(self, ex, ey, x_indeces, y_indeces):
         C = self.cos(ex.unsqueeze(1), ey)
 
-        C1, C2 = self.N.log_prob(C.max(dim=-1).values[x_indeces]), self.N.log_prob(C.max(dim=0).values[y_indeces])
+        C1, C2 = self.N.log_prob(1+C.max(dim=-1).values[x_indeces]), self.N.log_prob(1+C.max(dim=0).values[y_indeces])
         return -(torch.exp(C1) * C1).sum(), -(torch.exp(C2) * C2).sum()
